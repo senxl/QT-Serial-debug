8282e24a9539b8704e68a136c4add1cb74866a6f
senxl <1948145698@qq.com>
2020-11-27 10:55:11 +0800

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,158 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //默认选择COM1
+    ui->combox->setCurrentIndex(1);
+    //按钮响应
+    PushButtonConnect();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::PushButtonConnect()
+{
+    connect(ui->pb_start,&QPushButton::clicked,this,&MainWindow::pb_start);
+    connect(ui->pb_stop,&QPushButton::clicked,this,&MainWindow::pb_stop);
+    connect(ui->pb_send,&QPushButton::clicked,this,&MainWindow::pb_send);
+    connect(ui->pb_clear,&QPushButton::clicked,this,&MainWindow::pb_clear);
+    connect(ui->theme,&QPushButton::clicked,this,&MainWindow::theme);
+    connect(ui->hexsend,&QPushButton::clicked,this,&MainWindow::hexsend);
+
+}
+void MainWindow::pb_start()
+{
+    //获取可用串口
+    QList<QSerialPortInfo> port = qspi->availablePorts();
+    //选择连接串口
+    for (int i=0;i<port.length();i++) {
+        if(port.at(i).portName()==ui->combox->currentText())
+        {
+            //串口号设置
+            qsp->setPort(port.at(i));
+            //串口波特率设置
+            qsp->setBaudRate(ui->baudbox->currentText().toInt());
+            //串口数据位设置
+            qsp->setDataBits(QSerialPort::DataBits(ui->databox->currentText().toInt()));
+            //串口停止位设置
+            qsp->setStopBits(QSerialPort::StopBits(ui->stopbox->currentIndex()));
+            //串口校验位设置
+            qsp->setParity( (QSerialPort::Parity)(ui->checkbox->currentIndex()<1 ? \
+                                                      ui->checkbox->currentIndex() : \
+                                                      ui->checkbox->currentIndex()+1) );
+            //开启串口
+            qsp->open(QIODevice::ReadWrite);
+            //监听串口 读取数据
+            connect(qsp,&QSerialPort::readyRead,this,&MainWindow::read_port);
+        }
+    }
+}
+void MainWindow::pb_stop()
+{
+    //串口关闭
+    qsp->close();
+}
+void MainWindow::pb_send()
+{
+    //判断发送类型
+    if(ui->hexsend->isChecked()){
+        //16进制转文本发送
+        QString msg = ui->textEdit->toPlainText();
+        QByteArray arr;
+        bool ok = HexToString(msg,arr);
+        if(ok){
+            qsp->write(arr);
+        }
+    }else{
+        //直接发送文本
+        QString msg = ui->textEdit->toPlainText();
+        qsp->write(msg.toUtf8());
+    }
+}
+void MainWindow::read_port()
+{
+    //读取收到的串口数据
+    QByteArray msg = qsp->readAll();
+
+    if(msg.length() != 0)
+    {
+        //实现自动显示到最新内容
+        ui->textBrowser->moveCursor(QTextCursor::End);
+        //判断输出类型（16进制 or 文本）
+        if(ui->hexdisplay->isChecked()){
+            ui->textBrowser->append(msg.toHex(' '));
+        }else{
+            ui->textBrowser->insertPlainText(QString::fromLocal8Bit(msg));
+        }
+    }
+}
+
+void MainWindow::pb_clear()
+{
+    //清屏保留样式表
+    ui->textBrowser->clear();
+    theme();
+}
+void MainWindow::theme()
+{
+    //修改样式表实现不同显示效果
+    if(ui->theme->isChecked())
+    {
+        ui->textBrowser->setStyleSheet("background-color: black; color:#FFF;");
+    }else{
+        ui->textBrowser->setStyleSheet("background-color: white; color:#000;");
+    }
+}
+void MainWindow::hexsend()
+{
+    //修改内容实现不同显示
+    if(ui->hexsend->isChecked())
+    {
+        //16进制显示
+        QString msg = ui->textEdit->toPlainText();
+        ui->textEdit->setText(msg.toUtf8().toHex(' '));
+    }else{
+        //文本显示
+        QString msg = ui->textEdit->toPlainText();
+        QByteArray arr;
+        bool ok = HexToString(msg,arr);
+        if(ok){
+          ui->textEdit->setText(arr);
+        }
+    }
+}
+//实现16进制转为字符串//
+//返回值 bool类型 ：true转换成功  false转换失败并且弹出提示框
+//形参 QString,QByteArray 类型：
+//                  msg 需要转换的16进制字符串，如 "33 34 35"
+//                  ret 返回转换结果 ,  如 "345"
+bool MainWindow::HexToString(QString &msg,QByteArray &ret)
+{
+    QByteArray arr;
+    bool ok;
+    QStringList sl = msg.split(' ');
+    foreach (QString s,sl){
+        if(!s.isEmpty()){
+            char c = s.toInt(&ok,16)&0xFF;
+            if(ok){
+                arr.append(c);
+            }else{
+                QMessageBox::warning(this,"错误",QString("无效的16进制\'%1\'").arg(s));
+                ok = false;
+                break;
+            }
+            ok = true;
+        }
+    }
+    if(ok){
+        ret = arr;
+    }
+    return ok;
+}
---
+++ mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QDebug>
+#include <QMessageBox>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QSerialPort *qsp = new QSerialPort();
+    QSerialPortInfo *qspi = new QSerialPortInfo();
+
+    void PushButtonConnect();
+public slots:
+    void pb_start();
+    void pb_stop();
+    void pb_send();
+    void read_port();
+    void pb_clear();
+    void theme();
+    void hexsend();
+    bool HexToString(QString &msg,QByteArray &arr);
+
+};
+#endif // MAINWINDOW_H
